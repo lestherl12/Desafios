a4eac97d67d6108eec186cd241db4ff2adf6c8ee
lestherl12 <106944199+lestherl12@users.noreply.github.com>
2023-03-03 23:10:20 -0600

    Add files via upload

---
+++ Desafiocinco/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Program
+{
+    static int Puntaje = 0;
+    static int PuntuacionRecord = 0;
+    static string JugadorRecord = "";
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese el nombre del jugador 1: ");
+        string NombredelJugador1 = Console.ReadLine();
+
+        Console.WriteLine("Ingrese el puntaje de jugador 1: ");
+        int PuntuaciondelJugador1 = Int32.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el nombre de jugador 2: ");
+        string NombredelJugador2 = Console.ReadLine();
+
+        Console.WriteLine("Ingrese el puntaje de jugador 2: ");
+        int PuntuaciondelJugador2 = Int32.Parse(Console.ReadLine());
+
+        CheckScore(PuntuaciondelJugador1, NombredelJugador1);
+        CheckScore(PuntuaciondelJugador2, NombredelJugador2);
+    }
+
+    static void CheckScore(int Puntaje, string Nombre)
+    {
+        if (Puntaje > PuntuacionRecord)
+        {
+            PuntuacionRecord = Puntaje;
+            JugadorRecord = Nombre;
+
+            Console.WriteLine("La nueva puntuación más alta es " + PuntuacionRecord);
+            Console.WriteLine("La puntuación más alta fue lograda por " + JugadorRecord);
+        }
+        else
+        {
+            Console.WriteLine("La puntuación más alta de " + PuntuacionRecord + " no se ha podido superar, y aún está en manos de " + JugadorRecord);
+        }
+    }
+}
